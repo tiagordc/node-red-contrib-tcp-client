c00b0cc13b478e5a09b2fdca9ce63f3643425d31
Tiago Costa <tiagorc@gmail.com>
2019-10-21 11:43:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# node-red-contrib-tcp-client
+
+Unlike the default node-red tcp-in node, this one allows to create a dynamic TCP client.
+
+It also allows XML output.
+
+## How it works:
+
+![node configuration](https://raw.githubusercontent.com/tiagordc/node-red-contrib-simple-soap/master/edit.png)
+
+### Future:
+
+Reset connection
+
+## To install: 
+
+Install node-red.
+
+Install this package with "npm install node-red-contrib-tcp-client --save" in ~./node-red or via the Palette Manager in node-red.
+
+If everything was successfull you should see the new tcp node under the network category.
+
+https://flows.nodered.org/node/node-red-contrib-tcp-client
---
+++ tcp-client.js
@@ -0,0 +1,203 @@
+'use strict';
+
+module.exports = function (RED) {
+
+    //https://github.com/node-red/node-red/blob/master/packages/node_modules/%40node-red/nodes/core/network/31-tcpin.js
+
+    var socketTimeout = RED.settings.socketTimeout||null;
+
+    function TcpClient(config) {
+
+        var net = require('net'); //https://nodejs.org/api/net.html
+
+        RED.nodes.createNode(this, config);
+
+        this.action = config.action;
+        this.port = config.port * 1;
+        this.topic = config.topic;
+        this.stream = (!config.datamode || config.datamode=='stream'); /* stream,single*/
+        this.datatype = config.datatype || 'buffer'; /* buffer,utf8,base64,xml */
+        this.newline = (config.newline || "").replace("\\n","\n").replace("\\r","\r");
+        this.closing = false;
+        this.connected = false;
+
+        const node = this;
+        var connectionPool = {};
+
+        node.on('input', function (msg, nodeSend, nodeDone) {
+
+            if (config.actionType === 'msg' || config.actionType === 'flow' || config.actionType === 'global') {
+                node.action = RED.util.evaluateNodeProperty(config.action, config.actionType, this, msg);
+            }
+
+            if (config.portType === 'msg' || config.portType === 'flow' || config.portType === 'global') {
+                node.port = (RED.util.evaluateNodeProperty(config.port, config.portType, this, msg)) * 1;
+            }
+
+            var server = net.createServer(function (socket) {
+
+                socket.setKeepAlive(true, 120000);
+
+                if (socketTimeout !== null) {
+                    socket.setTimeout(socketTimeout);
+                }
+
+                var id = (1 + Math.random() * 4294967295).toString(16);
+                connectionPool[id] = socket;
+
+                var buffer = (node.datatype == 'buffer') ? Buffer.alloc(0) : "";
+
+                socket.on('data', function (data) {
+
+                    if (node.datatype != 'buffer') {
+                        data = data.toString(node.datatype == 'xml' ? 'utf8' : node.datatype);
+                    }
+
+                    if (node.stream) {
+
+                        var result = {
+                            topic: msg.topic || config.topic
+                        };
+
+                        if ((typeof data) === "string" && node.newline !== "") {
+
+                            buffer = buffer + data;
+                            var parts = buffer.split(node.newline);
+
+                            for (var i = 0; i < parts.length - 1; i += 1) {
+                                
+                                result.payload = parts[i];
+
+                                if (node.datatype == 'xml') {
+
+                                    var xml2js = require('xml2js');
+                                    var parseXml = xml2js.parseString;
+                                    var parseOpts = {};
+                                    parseOpts.async = true;
+                                    parseOpts.attrkey = '$';
+                                    parseOpts.charkey = '_';
+
+                                    if (config.xmlStrip) {
+                                        var stripPrefix = require('xml2js').processors.stripPrefix;
+                                        parseOpts.tagNameProcessors = [ stripPrefix ];
+                                    }
+
+                                    parseXml(result.payload + node.newline, parseOpts, function (parseErr, parseResult) {
+                                        if (!parseErr) { 
+                                            result.payload = config.xmlSimplify ? simplifyXML(parseResult) : parseResult;
+                                            nodeSend(result);
+                                        }
+                                    });
+
+                                }
+                                else {
+                                    nodeSend(result);
+                                }
+
+                            }
+
+                            buffer = parts[parts.length - 1];
+
+                        }
+                        else {
+                            result.payload = data;
+                            nodeSend(result);
+                        }
+
+                    }
+                    else {
+
+                        if ((typeof data) === "string") {
+                            buffer = buffer + data;
+                        }
+                        else {
+                            buffer = Buffer.concat([buffer, data], buffer.length + data.length);
+                        }
+
+                    }
+
+                });
+
+                socket.on('end', function () {
+                    if (!node.stream || (node.datatype === "utf8" && node.newline !== "")) {
+                        if (buffer.length > 0) {
+                            var result = {
+                                topic: msg.topic || config.topic,
+                                payload: buffer
+                            };
+                            nodeSend(result);
+                        }
+                        buffer = null;
+                    }
+                });
+
+                socket.on('timeout', function () {
+                    socket.end();
+                });
+
+                socket.on('close', function () {
+                    delete connectionPool[id];
+                });
+
+                socket.on('error', function (err) {
+                    node.log(err);
+                });
+
+            });
+           
+            server.on('error', function (err) {
+                if (err) {
+                    node.error(err);
+                }
+            });
+
+            server.listen(node.port, function (err) {
+                if (err) {
+                    node.error(err);
+                } else {
+                    node.on('close', function () {
+                        for (var c in connectionPool) {
+                            if (connectionPool.hasOwnProperty(c)) {
+                                connectionPool[c].end();
+                                connectionPool[c].unref();
+                            }
+                        }
+                        node.closing = true;
+                        server.close();
+                    });
+                }
+            });
+
+        });
+
+        node.on("close",function() {
+            node.status({});
+        });
+
+        function simplifyXML(message) {
+    
+            if (typeof message !== 'object') 
+                return message;
+            
+            var keys = Object.keys(message);
+            var result = message;
+            
+            if (keys.length === 1 && keys[0] === '0') { // instanceOf Array
+                result = result['0'];     
+            }
+    
+            if (typeof result === 'object')  {
+                for (var prop in result) {
+                    result[prop] = simplifyXML(result[prop]);
+                }
+            }
+          
+            return result;
+              
+        }
+
+    };
+
+    RED.nodes.registerType("tcp-client", TcpClient);
+
+};
